9d926e6fb8e0d4a04c939bf7a79cd207559e1559
Jmjimmy20 <you@example.com>
2020-03-25 14:33:07 -0500

    back

--- src/main/java/com/edu/eci/arsw/covid/persistance/CoronavirusStatsCache.java
+++ src/main/java/com/edu/eci/arsw/covid/persistance/CoronavirusStatsCache.java
@@ -2,16 +2,19 @@ package com.edu.eci.arsw.covid.persistance;
 
 import org.springframework.stereotype.Service;
 
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import com.edu.eci.arsw.covid.models.Country;
 
 @Service
 public class CoronavirusStatsCache{
-    ConcurrentHashMap<String, CachePaises> cache;
+    HashMap<String, CachePaises> cache;
+
+    HashMap<String, ArrayList<Country>> provincias;
 
     public CoronavirusStatsCache(){
-        cache = new ConcurrentHashMap<>();
+        cache = new HashMap<>();
     }
 
     public void addNewCountry(String key, Country data){
@@ -22,6 +25,24 @@ public class CoronavirusStatsCache{
     public CachePaises getCache(String key){
 
         return cache.get(key);
+    }
+
+    public void addCacheProv(String key,ArrayList<Country> array){
+        provincias.put(key, array);
+    }
+
+    public ArrayList<Country> getCacheProv(String key){
+
+
+        return provincias.get(key);
+    }
+
+    public HashMap<String, CachePaises> getCache(){
+        return cache;
+    }
+
+    public boolean isEmpty(){
+        return cache.isEmpty();
     }
     
 }
--- src/main/java/com/edu/eci/arsw/covid/services/impl/CoronavirusStatsServicesImpl.java
+++ src/main/java/com/edu/eci/arsw/covid/services/impl/CoronavirusStatsServicesImpl.java
@@ -1,9 +1,11 @@
 package com.edu.eci.arsw.covid.services.impl;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 import com.edu.eci.arsw.covid.models.Country;
+import com.edu.eci.arsw.covid.persistance.CachePaises;
 import com.edu.eci.arsw.covid.persistance.CoronavirusStatsCache;
 import com.edu.eci.arsw.covid.services.CoronavirusStatsServices;
 import com.edu.eci.arsw.covid.services.HttpConnectionService;
@@ -29,33 +31,47 @@ public class CoronavirusStatsServicesImpl implements CoronavirusStatsServices{
     @Override
     public HashMap<String, Country> getCountries() {
         HashMap <String, Country> datos = new HashMap<String, Country>();
-        try{  
-            paises = httpCS.getAllCou();
-            processData(paises, "country", datos);
-        }catch(UnirestException e){
-            e.printStackTrace();
+        if(cacheP.isEmpty()){
+            try{  
+                paises = httpCS.getAllCou();
+                processData(paises, "country", datos);
+            }catch(UnirestException e){
+                e.printStackTrace();
+            }
+            for(Map.Entry<String, Country> c : datos.entrySet()){
+                cacheP.addNewCountry(c.getKey(), c.getValue());
+            }
+        }else{
+            for(Map.Entry<String, CachePaises> c : cacheP.getCache().entrySet()){
+                datos.put(c.getKey(), c.getValue().getData());
+            }
         }
-        for(Map.Entry<String, Country> c : datos.entrySet()){
-            cacheP.addNewCountry(c.getKey(), c.getValue());
-        }
+        
         return datos;
     }
 
     @Override
     public HashMap<String, Country> getCountry(String country)  {
         HashMap <String, Country> datos = new HashMap<String, Country>();
-
-            try {
-                paises = httpCS.getCountryStats(country);
-                //System.out.println(paises.substring(50));
-                processData(paises, "province", datos);
-            } catch (UnirestException e) {
-                e.printStackTrace();
+            if(cacheP.getCacheProv(country).isEmpty()){
+                try {
+                    paises = httpCS.getCountryStats(country);
+                    //System.out.println(paises.substring(50));
+                    processData(paises, "province", datos);
+                } catch (UnirestException e) {
+                    e.printStackTrace();
+                }
+                ArrayList<Country> array = new ArrayList<Country>();
+                for(Map.Entry<String, Country> c : datos.entrySet()){
+                    array.add(c.getValue());
+                }
+                cacheP.addCacheProv(country, array);
+            }else{
+                for(Country c : cacheP.getCacheProv(country)){
+                    datos.put(c.getCountry(), c);
+                }
             }
             
-            for(Map.Entry<String, Country> c : datos.entrySet()){
-                cacheP.addNewCountry(c.getKey(), c.getValue());
-            }
         return datos;
     }
 
